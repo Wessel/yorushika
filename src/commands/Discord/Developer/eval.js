32da97dc6d98ba485667f7f5e8bc32a578b0d75b
Wessel T <discord@go2it.eu>
2019-02-15 22:20:59 +0100

    Remove inspect

--- src/commands/Discord/Developer/eval.js
+++ src/commands/Discord/Developer/eval.js
@@ -36,7 +36,7 @@ module.exports = class Eval extends DiscordCommand {
     catch (ex) { result = ex; errored = true; }
     finally {
       if (silent) return message.edit(this.localize(msg.author.locale['developer']['eval']['silent']));
-      result = this.bot.util.escapeMarkdown(this.sanitize(String(inspect(result))));
+      result = this.bot.util.escapeMarkdown(this.sanitize(String(result)));
 
       message.edit({
         content: '',
